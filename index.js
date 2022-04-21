e71a1081835057a1e33d4a702e72660580a1366c
yasserhcn <hacini_yasser@yahoo.fr>
2022-04-22 00:13:17 +0100

    added function to load json file

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ http.hostname = 'localhost';
 http.port = 80;
 
 http.createServer((req, res)=>{
+
+    //* main index path
     if(req.url == '/'){
         res.writeHead(200, {'content-type' : 'text/html'});
         let page = fs.readFileSync('pages/main.html');
+        res.write(page);
+        res.end();
+    }else
+    //* link1 request url
+    if(req.url == "/link1"){
+        res.writeHead(200, {'content-type' : 'text/html'});
+        let page = fs.readFileSync('pages/main.html');
+        loadLesson("alg/lesson.json", (err, txt)=>
+        {
+            console.log(txt);
+        });
         res.write(page);
         res.end();
     }
@@ -24,3 +37,15 @@ http.createServer((req, res)=>{
     console.log(req.url);
     
 }).listen(80)
+
+function loadLesson(lessonName, result)
+{
+    if(typeof(lessonName) != 'string'){
+        result(new Error("lessonName is not a string"));
+    }else
+    {
+        console.log("lskqfmlqksfmlqksfmlqksdmfl");
+        let file = fs.readFileSync("./db/" + lessonName);
+        result(null, String(file));
+    }
+}
